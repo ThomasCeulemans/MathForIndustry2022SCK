ef6a0f4be5ae209bab244b3d6e4e3cd09dfda798
Thomas Ceulemans <thomas.ceulemans@kuleuven.be>
2022-09-20 11:33:25 +0200

    Corrected possibly incorrect version of plotting the simulated observations.

--- code/import.py
+++ code/import.py
@@ -9,10 +9,24 @@ MATRIX_LOCATION = "./../data/srs_csv_files/"
 scalings=pandas.read_csv("./../data/scalings.csv", header=None)
 sources=pandas.read_csv("./../data/data_xe133_NH_yearly.csv")#has a header file, not that we actually need it
 
-np_scalings=scalings.to_numpy()
+print(scalings)
+print(sources)
+
+np_scalings=scalings[0].to_numpy()#/86400#number of seconds in a day
 np_sources=sources['emission'].to_numpy()
+print(np_sources.size)
+print(np_sources.shape)
 #rescaling sources
+print("scalings: ", np_scalings)
+print("before dividing: ", np_sources)
 np_sources=np_sources/np_scalings
+print("after dividing:", np_sources)
+#np.divide(np_sources,np_scalings)
+
+
+print(np_scalings.size)
+print(np_scalings.shape)
+print("np sources: ", np_sources.size)
 
 n_datafiles=0
 for file in os.listdir(MATRIX_LOCATION):
@@ -22,6 +36,13 @@ results=np.zeros(n_datafiles)
 print(results)
 
 index=0
+
+#timesteps times number of emissions
+nb_data_cols=np.size(np_sources)
+print(nb_data_cols)
+
+combined_dataframe=pandas.DataFrame()
+
 for file in os.listdir(MATRIX_LOCATION):
     if file.endswith(".csv"):
         matrix_file = pandas.read_csv(os.path.join(MATRIX_LOCATION, file), header=None)
@@ -34,9 +55,14 @@ for file in os.listdir(MATRIX_LOCATION):
         date = split_name[1]
         time = split_name[2]
 
-        #checking whether everything sums to 1 (or something negative)
+        # print("matrix: ", np_matrix.shape)
 
-        result=np.sum(np.matmul(np_sources, np_matrix))
+
+
+        #checking whether everything sums to 1 (or something negative)
+        # print(np.matmul(np_matrix.T, np_sources))
+        result=np.sum(np.matmul(np_matrix.T, np_sources))
+        # print()
         results[index]=result
 
         index+=1
@@ -50,4 +76,7 @@ print(scalings)
 print(sources)
 
 plt.hist(np.log10(results), bins=50)
+plt.xlabel("log10(y_sim)")
+plt.legend()
+plt.title("Simulated observations")
 plt.show()
