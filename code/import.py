10f8669091232ea23a483a39812c4c54249ceb15
Thomas Ceulemans <thomas.ceulemans@kuleuven.be>
2022-09-20 10:03:14 +0200

    Plotting the simulated emissions

--- code/import.py
+++ code/import.py
@@ -1,6 +1,7 @@
 import pandas
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 MATRIX_LOCATION = "./../data/srs_csv_files/"
 
@@ -8,20 +9,37 @@ MATRIX_LOCATION = "./../data/srs_csv_files/"
 scalings=pandas.read_csv("./../data/scalings.csv", header=None)
 sources=pandas.read_csv("./../data/data_xe133_NH_yearly.csv")#has a header file, not that we actually need it
 
+np_scalings=scalings.to_numpy()
+np_sources=sources['emission'].to_numpy()
+#rescaling sources
+np_sources=np_sources/np_scalings
+
+n_datafiles=0
+for file in os.listdir(MATRIX_LOCATION):
+    if file.endswith(".csv"):
+        n_datafiles+=1
+results=np.zeros(n_datafiles)
+print(results)
+
+index=0
 for file in os.listdir(MATRIX_LOCATION):
     if file.endswith(".csv"):
         matrix_file = pandas.read_csv(os.path.join(MATRIX_LOCATION, file), header=None)
-        #TODO: find date and location from filename
+        np_matrix = matrix_file.to_numpy()
         #prune extension from filename
         pruned_name = file.split('.')[0]
         split_name = pruned_name.split('_')
-        print(split_name)
+        # print(split_name)
         location = split_name[0]
         date = split_name[1]
         time = split_name[2]
 
+        #checking whether everything sums to 1 (or something negative)
 
+        result=np.sum(np.matmul(np_sources, np_matrix))
+        results[index]=result
 
+        index+=1
         # print(os.path.join(MATRIX_LOCATION, file))
         # print(matrix_file)
 
@@ -30,3 +48,6 @@ for file in os.listdir(MATRIX_LOCATION):
 
 print(scalings)
 print(sources)
+
+plt.hist(np.log10(results), bins=50)
+plt.show()
