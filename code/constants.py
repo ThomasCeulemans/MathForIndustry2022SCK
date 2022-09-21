fcd8143396e7eede333eaa5fed7666423ac1137c
jerome-ugent <114058197+jerome-ugent@users.noreply.github.com>
2022-09-21 16:19:51 +0200

    Delete code directory

--- code/constants.py
+++
@@ -1,6 +0,0 @@
-#Just a file defining some constants for these scripts
-LIMIT_BACKWARD_TIME = 15
-N_SOURCES = 200
-N_DETECTIONS = 4636
-#allowing us to use values for 1 entire year
-N_COLS=365+LIMIT_BACKWARD_TIME
