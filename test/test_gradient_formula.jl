d37660608c66721cc3a56895372b96d660b16aa5
Nick Dewaele <nick.dewaele@kuleuven.be>
2022-09-21 12:58:23 +0200

    reformulate theory using W = exp(V)

--- test/test_gradient_formula.jl
+++ test/test_gradient_formula.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra, Zygote
 
+
 # Generate simplified version of the data
 Iᵈᵉᵗ = [1,3,4] # indices corresponding to detection events
 Mₛ = abs.(randn(4, 10)) # M_shift (structure unimportant)
@@ -8,18 +9,17 @@ x̃  = exp.(randn(size(Mₛ,2)))
 λ  = 0.1
 
 norm²(x) = dot(x,x) # automatically differentiable
-f(W :: AbstractMatrix) = norm²(log.(P*Mₛ*W*x̃))/2 + λ*norm²(log.(diag(W)))/2 # function of diagonal matrix W
-f(w :: AbstractVector) = f(Diagonal(w)) # function of vector of diagonal elements
+v₀ = randn(size(x̃))
+W₀ = exp(Diagonal(v₀))
 
-# compute gradient using automatic differentiation
-w₀ = exp.(randn(size(x̃)))
-W₀ = Diagonal(w₀)
-∇fᴬᴰ = Diagonal(Zygote.gradient(f, w₀)[1])
+# gradient using automatic differentiation
+f(v) = norm²(log.(P*Mₛ*exp(Diagonal(v))*x̃))/2 + λ*norm²(v)/2	
+∇fᴬᴰ = Zygote.gradient(f, v₀)[1]
 
-# gradient obtained from first principles
+# gradient using first principles derivation
 ŷ = Mₛ*W₀*x̃
 ẑ = [ i ∈ Iᵈᵉᵗ ? log(ŷ[i]) / ŷ[i] : 0.0 for i ∈ axes(Mₛ,1)]
-∇f = Diagonal((Mₛ'*ẑ) .* x̃) + λ * (W₀ \ log(W₀))
+∇f = exp.(v₀) .* (Mₛ'*ẑ) .* x̃ + λ * v₀
 
 println("|| ∇f || = ", norm(∇f))
 println("|| ∇fᴬᴰ || = ", norm(∇fᴬᴰ))
