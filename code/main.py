1e24a667d373696686429679d9d01294f90be0c6
Thomas Ceulemans <thomas.ceulemans@kuleuven.be>
2022-09-21 09:38:00 +0200

    Splitted file for code clarity.

---
+++ code/main.py
@@ -0,0 +1,23 @@
+import readin as read
+import analysis as analysis
+import numpy as np
+import constants as cst
+import matplotlib.pyplot as plt
+
+np_sources = read.import_scaled_sources()
+print("importing matrix")
+#import the combined matrix
+giant_matrix = read.import_combined_matrix()
+print("done importing matrix")
+#and throw away all zero/negative rows
+pruned_giant_matrix = read.prune_matrix(giant_matrix)
+
+#default source assumes all sources to be constant at all times
+default_x = np.kron(np_sources, np.ones(cst.N_COLS))
+y_sim = analysis.evaluate_x(pruned_giant_matrix, default_x)
+
+plt.hist(np.log10(y_sim), bins=50)
+plt.xlabel("log10(y_sim)")
+plt.legend()
+plt.title("Simulated observations")
+plt.show()
